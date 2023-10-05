f33a809d94cb4c8bc6f3ec4b22a2e65d062787d7
tom-avilius <aaarshdwivedi244475@gmail.com>
2023-10-06 00:57:04 +0530

    Add overlay 5

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,2 +1,22 @@
 
-window.alert('Hola');
+// changing the showcase 4 background every second
+const showcase4 = document.getElementsByClassName('showcase-4')[0];
+var toggleCount = 0; //toggle variable
+
+// function to change the background of showcase 4
+function changeBackgroundShowcase4() {
+
+    if (toggleCount%2 == 0) {
+
+        showcase4.style.backgroundImage = "url('./assets/img/S4.jpg')";
+    } else {
+
+        showcase4.style.backgroundImage = "url('./assets/img/S5.jpg')";
+    }
+
+    toggleCount++;
+
+    setTimeout(changeBackgroundShowcase4, 2000);
+}
+
+changeBackgroundShowcase4();
